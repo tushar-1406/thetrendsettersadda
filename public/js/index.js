51e9031ee8f5ec29bbc40c23ac74c6028f8a5db6
Tushar Popli <tusharpopli1406@gmail.com>
2022-02-09 23:36:04 +0530

    changed

--- public/js/index.js
+++ public/js/index.js
@@ -33,29 +33,30 @@ function getProducts(){
         let html=``;
         for(var i=0;i<4;i++)
         {
-            html+=`<div class="col-lg-3 col-sm-6 col-xs-12 mt-5 mb-4 products"  style="cursor: pointer;" id="${products[i]._id}">
+            html+=`<a><div class="col-lg-3 col-sm-6 col-xs-12 mt-5 mb-4 products"  style="cursor: pointer;" id="${products[i]._id}">
             <img src="uploads/${products[i].images[0].image}" class="image-fluid" width="230" height="230" alt="">
             <p class="mt-2 mb-0">${products[i].productName}</p>
             <p class="mt-0 mb-0">₹ ${products[i].price}</p>
             <p class="mt-0 mb-0" style="font-size: 2vh;">Free delievery</p>
             </div>
-            
+            </a>
             `
         }
         productscontainer.innerHTML=html;
         let allproducts=document.querySelectorAll(".products");
         for(var i=0;i<allproducts.length;i++)
         {
-            // allproducts[i].addEventListener("click",function(product){     
+            allproducts[i].addEventListener("click",function(product){     
                 
-            //     productDetail(product.path[1].id);
-                
-               
-            // })
-            allproducts[i].onclick = function (product) {
                 productDetail(product.path[1].id);
                 
-            }
+               
+            })
+            // allproducts[i].onclick = function (product) {
+            //     console.log("hello");
+            //     productDetail(product.path[1].id);
+                
+            // }
             
         }
     })
